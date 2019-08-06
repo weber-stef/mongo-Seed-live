2160cb0798d4bf7f794a3da6f55c30c85f8dbf5b
Leandro <leandro.frigerio@devugees.org>
2019-08-06 11:10:28 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const faker = require("faker");
+
+const UserModel = require("./models/user");
+
+mongoose.connect("mongodb://localhost:27017/seed-test", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+const createFakeUsers = (amount = 5) =>
+    new Array(amount).fill(0).map(() => ({
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        age: Math.floor(Math.random() * 70 + 18),
+        username: faker.internet.userName(),
+        short_bio: faker.lorem.text(),
+        address: {
+            street: faker.address.streetName(),
+            street_number: Math.floor(Math.random() * 7000 + 0),
+            zip: faker.address.zipCode(),
+            city: faker.address.city(),
+            country: faker.address.country()
+        },
+        phone_number: faker.phone.phoneNumber()
+    }));
+
+const main = async () => {
+    await db.dropCollection("users");
+    const fakeUser = createFakeUsers(1000);
+    const fakeUserModels = fakeUser.map(user => new UserModel(user).save());
+
+    await Promise.all(fakeUserModels);
+    console.log("Done");
+    process.exit(0);
+};
+
+main();
